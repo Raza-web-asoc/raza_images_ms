ace4dca4c3ce03b2f496af7081b0d4f9b1a49e65
sergiocastro-cw <sergio.castro@caseware.com>
2025-01-09 19:52:36 -0500

    MEJORAS

--- app/src/database/database.py
+++ app/src/database/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from app.src.database.base_class import Base
 
 from dotenv import load_dotenv
--- app/src/main.py
+++ app/src/main.py
@@ -2,11 +2,21 @@ from fastapi import FastAPI, Depends
 from app.src.routes import users_router
 from app.src.routes import pets_router
 from app.src.database.database import init_db
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Orígenes permitidos
+    allow_credentials=True,                  # Permitir cookies/autenticación
+    allow_methods=[""],                     # Permitir todos los métodos HTTP
+    allow_headers=[""],                     # Permitir todos los encabezados
+)
+
 init_db()
+
 
 app.include_router(users_router)
 app.include_router(pets_router)
