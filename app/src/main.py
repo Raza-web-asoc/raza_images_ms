f5aef92dbb8fc4057cd9eb27d18dd7e069bf1bb2
Juan Ovalle <jpovalleg@gmail.com>
2025-02-28 20:22:19 -0500

    Added prometheus middleware

--- app/src/main.py
+++ app/src/main.py
@@ -3,6 +3,7 @@ from app.src.routes import users_router
 from app.src.routes import pets_router
 from app.src.database.database import init_db
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 app = FastAPI()
@@ -15,12 +16,17 @@ app.add_middleware(
     allow_headers=[""],                     # Permitir todos los encabezados
 )
 
+instrumentator = Instrumentator(should_group_status_codes=False).instrument(app)
+instrumentator.expose(app, endpoint="/metrics")
 init_db()
 
 
 app.include_router(users_router)
 app.include_router(pets_router)
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Images API"}
 
 
 
